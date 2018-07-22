587f414ebb781311a257d05e825a51de7cd22865
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-22 15:51:09 +0530

    Add files via upload

---
+++ pangram.java
@@ -0,0 +1,20 @@
+package player;
+
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+
+public class pangram {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String sen=in.nextLine().toLowerCase();
+	String al=sen.replaceAll(" ","");
+	char c1[]=al.toCharArray();
+	Set set = new HashSet();
+	 for(char c: c1){
+         set.add(c);
+     }
+	if(set.size()==26){System.out.println("yes");}
+	else{System.out.println("no");}
+}
+}
