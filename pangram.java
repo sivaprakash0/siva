50115e38986987cd5664de52dc4c1e6277f2cc16
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-22 15:51:31 +0530

    Update pangram.java

--- pangram.java
+++ pangram.java
@@ -1,9 +1,6 @@
-package player;
-
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
-
 public class pangram {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
