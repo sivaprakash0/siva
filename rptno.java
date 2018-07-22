a50745b731713d5f9f20954cf5f0282854f08c11
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-22 12:00:16 +0530

    Update rptno.java

--- rptno.java
+++ rptno.java
@@ -1,8 +1,5 @@
-package gold;
-
 import java.util.Arrays;
 import java.util.Scanner;
-
 public class rptno {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
@@ -10,14 +7,14 @@ public static void main(String[] args) {
 	int arr[]=new int[sz];
 	for(int i=0;i<sz;i++){
 		arr[i]=in.nextInt();
-	}
+	 }
 	Arrays.sort(arr);
 	String s="";
-	for(int j=0;j<sz-1;j++){
+	   for(int j=0;j<sz-1;j++){
 		if(arr[j]==arr[j+1]){
 			s=s+arr[j];
 		}
-	}
-	System.out.println(s.replaceAll(""," ").trim());
+	   }
+	  System.out.println(s.replaceAll(""," ").trim());
 }
 }
