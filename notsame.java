9408f52bd76c8d7c9dfc2c33216d39df27ad68bb
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-22 10:08:04 +0530

    Add files via upload

---
+++ notsame.java
@@ -0,0 +1,31 @@
+package gold;
+
+import java.util.Scanner;
+
+public class notsame {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String wrd=in.next();
+	String wrd1=in.next();
+	char c[]=wrd.toCharArray();
+	char c1[]=wrd1.toCharArray();
+	int cn=0;
+	if(wrd.length()<wrd1.length()){
+	for(int i=0;i<wrd.length();i++){
+		if(c[i]==c1[i]){
+			cn++;
+		}
+	}
+	System.out.println(wrd1.length()-cn);
+	}
+	else{
+			for(int i=0;i<wrd1.length();i++){
+				if(c[i]==c1[i]){
+					cn++;
+				}
+			}
+			System.out.println(wrd.length()-cn);
+			
+	}
+}
+}
