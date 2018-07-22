feb76be96f3a70083a54dc0751045378bacc3856
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-22 14:32:25 +0530

    Update stringencd.java

--- stringencd.java
+++ stringencd.java
@@ -1,23 +1,20 @@
-package gold;
-
 import java.util.Scanner;
-
 public class stringencd {
 public static void main(String[] args) {
 	Scanner in=new Scanner(System.in);
 	String w=in.next().toLowerCase();
-    String w1=in.next().toLowerCase();
-    String x="";
-    char c[]=w.toCharArray();
-    char c1[]=w1.toCharArray();
-    for(int i=0;i<w.length();i++){
-    	int s=((c[i])-96+(c1[i])-96)+96;
+         String w1=in.next().toLowerCase();
+          String x="";
+          char c[]=w.toCharArray();
+          char c1[]=w1.toCharArray();
+       for(int i=0;i<w.length();i++){
+    	   int s=((c[i])-96+(c1[i])-96)+96;
         if(s>122){
-        s=(s-122)+96;	
-        }
-        char a=(char)s;
-        x=x+a;
-    }
-   System.out.println(x);
-}
+           s=(s-122)+96;	
+          }
+          char a=(char)s;
+            x=x+a;
+       }
+     System.out.println(x);
+  }
 }
