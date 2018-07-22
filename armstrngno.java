9ed50b8061fa7f2edaaafae27ff940337ec74d3a
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-07-22 12:17:24 +0530

    Add files via upload

---
+++ armstrngno.java
@@ -0,0 +1,20 @@
+package set1;
+
+import java.util.Scanner;
+
+public class armstrngno {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	int no=in.nextInt();
+	String a=Integer.toString(no);
+	int mul,sum=0;
+	for(int i=0;i<a.length();i++){
+		char c=a.charAt(i);
+	    mul=(c-48)*(c-48)*(c-48);
+	    sum=sum+mul;
+	}
+	if(sum==no){
+	System.out.println("yes");}
+	else{System.out.println("no");}
+}
+}
