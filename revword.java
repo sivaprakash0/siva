5db216284dc404b42537ace9632be1c4b6891c0e
sivaprakash0 <31100688+sivaprakash0@users.noreply.github.com>
2018-08-15 14:12:36 +0530

    Add files via upload

---
+++ revword.java
@@ -0,0 +1,15 @@
+package set4;
+
+import java.util.Scanner;
+
+public class revword {
+public static void main(String[] args) {
+	Scanner in=new Scanner(System.in);
+	String s=in.nextLine();
+	String s1[]=s.split(" ");
+	for(String s2:s1){
+	String s3=new StringBuffer(s2).reverse().toString();
+	System.out.print(s3+" ");
+  }
+}	
+}
